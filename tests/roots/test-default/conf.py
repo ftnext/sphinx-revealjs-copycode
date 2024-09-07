844c3482cae276aa2d99ccee83ad3be07219b8f0
ftnext <takuyafjp+develop@gmail.com>
2024-09-07 10:41:49 +0900

    [docs] Show CopyCode plugin configuration

--- tests/roots/test-default/conf.py
+++ tests/roots/test-default/conf.py
@@ -2,3 +2,10 @@ extensions = [
     "sphinx_revealjs",
     "sphinx_revealjs_copycode",
 ]
+
+revealjs_script_plugins = [
+    {
+        "name": "CopyCode",
+        "src": "revealjs4/plugin/copycode/copycode.js",
+    },
+]
