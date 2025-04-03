53e10022fa7ea4ca342d13ecdaa955a34cbc6953
ftnext <takuyafjp+develop@gmail.com>
2025-04-03 23:23:37 +0900

    [bugfix] Pass v3 test

--- src/sphinx_revealjs_copycode/__init__.py
+++ src/sphinx_revealjs_copycode/__init__.py
@@ -9,6 +9,7 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 from sphinx.util import logging
+from sphinx.util.fileutil import copy_asset
 from sphinx.util.typing import ExtensionMetadata
 
 if TYPE_CHECKING:
@@ -18,39 +19,60 @@ __version__ = "0.2.0"
 logger = logging.getLogger(__name__)
 
 
+def copy_copycode_assets(app: Sphinx, exc):
+    if exc is not None:  # Build failed
+        return
+
+    plugin_dir_path = Path(__file__).parent / "_static"
+    plugin_dir_path.mkdir(parents=True, exist_ok=True)
+
+    if (plugin_dir_path / "copycode").exists():
+        logger.info("✅ Reveal.js CopyCode plugin is already installed")
+    else:
+        logger.info(
+            "Reveal.js CopyCode plugin is not yet installed. "
+            "Need to install it"
+        )
+        url = (
+            "https://github.com/Martinomagnifico/reveal.js-copycode/"
+            "archive/refs/tags/v1.2.0.zip"
+        )
+        with urlopen(url) as response:
+            bytes_stream = BytesIO(response.read())
+        with ZipFile(bytes_stream) as zf, TemporaryDirectory() as tmpdir:
+            tmpdir_path = Path(tmpdir)
+            for plugin_file in [
+                "copycode.css",
+                "copycode.esm.js",
+                "copycode.js",
+            ]:
+                zf.extract(
+                    f"reveal.js-copycode-1.2.0/plugin/copycode/{plugin_file}",
+                    path=tmpdir_path,
+                )
+            shutil.move(
+                tmpdir_path / "reveal.js-copycode-1.2.0/plugin/copycode",
+                plugin_dir_path,
+            )
+            logger.info("✅ Installed Reveal.js CopyCode plugin")
+
+    for asset in ["copycode.css", "copycode.esm.js", "copycode.js"]:
+        copy_asset(
+            plugin_dir_path / "copycode" / asset,
+            app.outdir
+            / "_static"
+            / "revealjs"
+            / "plugin"
+            / "copycode"
+            / asset,
+        )
+
+
 def setup(app: Sphinx) -> ExtensionMetadata:
     metadata = ExtensionMetadata(
         version=__version__, parallel_read_safe=False, parallel_write_safe=True
     )
 
-    plugin_dir_path = Path(__file__).parent / "_static"
-    plugin_dir_path.mkdir(parents=True, exist_ok=True)
-    app.config.html_static_path.append(str(plugin_dir_path.absolute()))
-
-    if (plugin_dir_path / "copycode").exists():
-        logger.info("✅ Reveal.js CopyCode plugin is already installed")
-        return metadata
-
-    logger.info(
-        "Reveal.js CopyCode plugin is not yet installed. Need to install it"
-    )
-    url = (
-        "https://github.com/Martinomagnifico/reveal.js-copycode/"
-        "archive/refs/tags/v1.2.0.zip"
-    )
-    with urlopen(url) as response:
-        bytes_stream = BytesIO(response.read())
-    with ZipFile(bytes_stream) as zf, TemporaryDirectory() as tmpdir:
-        tmpdir_path = Path(tmpdir)
-        for plugin_file in ["copycode.css", "copycode.esm.js", "copycode.js"]:
-            zf.extract(
-                f"reveal.js-copycode-1.2.0/plugin/copycode/{plugin_file}",
-                path=tmpdir_path,
-            )
-        shutil.move(
-            tmpdir_path / "reveal.js-copycode-1.2.0/plugin/copycode",
-            plugin_dir_path,
-        )
-        logger.info("✅ Installed Reveal.js CopyCode plugin")
+    app.connect("build-finished", copy_copycode_assets)
 
     return metadata
