bd80b210861aeda60835ef4aeafc268010a888b4
ftnext <takuyafjp+develop@gmail.com>
2025-04-07 08:35:58 +0900

    [refactor] Remove sphinx-revealjs v2 test env

--- tests/roots/test-sphinx-revealjs-v2/index.rst
+++
@@ -1,7 +0,0 @@
-Code block
-==========
-
-.. code-block:: python
-
-    while True:
-        print("Hello world!")
--- tests/test_sphinx_revealjs_copycode.py
+++ tests/test_sphinx_revealjs_copycode.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from importlib.metadata import version
 from typing import TYPE_CHECKING
 
 import pytest
@@ -30,27 +29,6 @@ def assert_copycode_static_files_exist(
     assert_file_exists(expected_copycode_directory / "copycode.esm.js")
 
 
-@pytest.mark.skipif(
-    not version("sphinx-revealjs").startswith("2."),
-    reason="requires sphinx-revealjs v2",
-)
-@pytest.mark.sphinx("revealjs", testroot="sphinx-revealjs-v2")
-def test_arrange_copycode_plugin_sphinx_revealjs_v2(
-    app: SphinxTestApp,
-) -> None:
-    app.build()
-
-    expected_copycode_directory = (
-        app.outdir / "_static/revealjs/plugin/copycode"
-    )
-    assert_directory_exists(expected_copycode_directory)
-    assert_copycode_static_files_exist(expected_copycode_directory)
-
-
-@pytest.mark.skipif(
-    not version("sphinx-revealjs").startswith("3."),
-    reason="requires sphinx-revealjs v3",
-)
 @pytest.mark.sphinx("revealjs", testroot="sphinx-revealjs-v3")
 def test_arrange_copycode_plugin_sphinx_revealjs_v3(
     app: SphinxTestApp,
